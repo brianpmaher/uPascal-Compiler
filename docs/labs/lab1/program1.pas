695cfe6298d0078dee79503ea2c867e0d565d29f
JesseStephenBrown <jessestephenbrown@gmail.com>
2015-01-23 16:15:12 -0700

    Add program1.pas

---
+++ docs/labs/lab1/program1.pas
@@ -0,0 +1,12 @@
+program Tester(input, output);
+
+  var I: Integer;
+
+    begin {tester}
+      Writeln; Writeln;
+      Write('Please enter an integer value for I: ');
+      Read(I);
+      I := I + 1;
+      Writeln('The current value of I is ', I:0);
+      Writeln; Writeln;
+    end. {tester}
