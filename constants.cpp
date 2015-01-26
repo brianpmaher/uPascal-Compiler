f829d3f4ff37d3154fd0413a7f848bc174eb82a5
Brian Maher <bmaher91@gmail.com>
2015-01-26 15:22:19 -0700

    1 - create YµP driver; also added some files from lab 1

---
+++ constants.cpp
@@ -0,0 +1,6 @@
+using System;
+
+// Contains all constants for our program. Put any large and/or repeated strings here.
+public static class Constants {
+	public const string USAGE_HELP = "Usage: mono YµP.exe <µPascal filename> <output filename>";
+}
