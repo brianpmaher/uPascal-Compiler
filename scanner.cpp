de05372a02d7aad530e379686743178ec742e904
Brian Maher <bmaher91@gmail.com>
2015-01-26 16:38:27 -0700

    added method templates and tokens documentation to docs

---
+++ scanner.cpp
@@ -0,0 +1,36 @@
+using System;
+
+public class Scanner {
+	public void initializeScanner(string file_name) {
+
+	}
+
+	private int getNextToken() {
+		return 0; // change this
+	}
+
+	private int getLineNumber() {
+		return 0; // change this
+	}
+
+	private int getColumnNumber() {
+		return 0; // change this
+	}
+
+	// Finite State Automatons
+	private int fsaLetter() {
+		return 0; // change this
+	}
+
+	private int fsaDigit() {
+		return 0; // change this
+	}
+
+	private int fsaPunct() { // doesn't include quotes
+		return 0; // change this
+	}
+
+	private int fsaString() { // surrounded by quotes
+		return 0; // change this
+	}
+}
