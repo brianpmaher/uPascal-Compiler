34fa0f289d2cc88f3cf32bc2b4599780531dd2f6
Brian Maher <bmaher91@gmail.com>
2015-01-26 16:10:08 -0700

    some changes

--- constants.cpp
+++ constants.cpp
@@ -3,4 +3,10 @@ using System;
 // Contains all constants for our program. Put any large and/or repeated strings here.
 public static class Constants {
 	public const string USAGE_HELP = "Usage: mono YµP.exe <µPascal filename> <output filename>";
+
+	// All token IDs (add more as we come across them)
+	public enum TOKENS {
+		AND,
+		BEGIN,
+	};
 }
