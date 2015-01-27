a154e04bcb7e6201e8ce4c95289cedcae923e456
jessestephenbrown <jessestephenbrown@gmail.com>
2015-01-26 20:50:19 -0700

    [WIP] Implement number scanner

--- constants.cpp
+++ constants.cpp
@@ -3,8 +3,15 @@ using System;
 // Contains all constants for our program. Put any large and/or repeated strings here.
 public static class Constants {
 	public const string USAGE_HELP = "Usage: mono YµP.exe <µPascal filename> <output filename>";
+	public const string DIGITS = "0123456789";
+	public const string LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
 	// All token IDs (add more as we come across them)
-	public enum TOKENS {
+	public enum TOKEN 
+	{
+		// Digit tokens
+		MP_INTEGER_LIT,
+		MP_FIXED_LIT,
+		MP_FLOAT_LIT
 	};
 }
