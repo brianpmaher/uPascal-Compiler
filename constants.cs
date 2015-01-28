728efe0e27c1140558b005f375c96388cb42f793
Brian Maher <bmaher91@gmail.com>
2015-01-27 17:45:54 -0700

    added all constants from documentation

--- constants.cs
+++ constants.cs
@@ -6,5 +6,70 @@ public static class Constants {
 
 	// All token IDs (add more as we come across them)
 	public enum TOKENS {
+		// Reserved Words
+		AND,			// "and"
+		BEGIN,			// "begin"
+		BOOLEAN,		// "boolean"
+		DIV,			// "div"
+		DO, 			// "do"
+		DOWNTO,			// "downto"
+		ELSE,			// "else"
+		END,			// "end"
+		FALSE,			// "False"
+		FIXED,			// "fixed"
+		FLOAT,			// "float"
+		FOR,			// "for"
+		FUNCTION,		// "function"
+		IF,				// "if"
+		INTEGER,		// "integer"
+		MOD,			// "mod"
+		NOT,			// "not"
+		OR,				// "or"
+		PROCEDURE,		// "procedure"
+		PROGRAM,		// "program"
+		READ,			// "read"
+		REPEAT,			// "repeat"
+		STRING,			// "string"
+		THEN,			// "then"
+		TRUE,			// "true"
+		TO,				// "to"
+		TYPE,			// "type"
+		UNTIL,			// "until"
+		VAR,			// "var"
+		WHILE,			// "while"
+		WRITE,			// "write"
+		WRITELN,		// "writeln"
+
+		// Identifiers and Literals
+		IDENTIFIER,		// (letter | "_"(letter | digit)){["_"](letter | digit)}
+		INTEGER_LIT,	// digit{digit}
+		FIXED_LIT,		// digit{digit} "." digit{digit}
+		FLOAT_LIT,		// (digit{digit} | digit{digit} "." digit{digit}) ("e"|"E")["+"|"-"]digit{digit}
+		STRING_LIT,		// "'" {"''" | AnyCharacterExceptApostropheOrEOL} "'"
+
+		// Single String Tokens
+		ASSIGN,			// ":="
+		COLON,			// ":"
+		COMMA,			// ","
+		EQUAL,			// "="
+		FLOAT_DIVIDE,	// "/"
+		GEQUAL,			// ">="
+		GTHAN,			// ">"
+		LEQUAL,			// "<="
+		LPAREN,			// "("
+		LTHAN,			// "<"
+		MINUS,			// "-"
+		NEQUAL,			// "<>"
+		PERIOD,			// "."
+		PLUS,			// "+"
+		RPAREN,			// ")"
+		SCOLON,			// ";"
+		TIMES,			// "*"
+
+		// Special Tokens
+		EOF,			// end-of-file character
+		RUN_COMMENT,	// run-on comment error
+		RUN_STRING,		// run-on string error
+		ERROR,			// token for other scan errors
 	};
 }
