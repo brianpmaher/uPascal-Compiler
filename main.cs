4b8d14842d5c7df39a42d4c79d268af63f51f85b
Brian Maher <bmaher91@gmail.com>
2015-02-06 14:45:23 -0700

    fixed usage for zero arguments

--- main.cs
+++ main.cs
@@ -4,27 +4,27 @@ using System.Collections.Generic;
 public class Driver {
     static public void Main(string[] args) {
         Scanner scanner = new Scanner();
-        if(args[0].Equals("-t")){
-            scanner.TestDriver();
-            Environment.Exit(0);
-        }
         // User entered incorrect usage for our program
-        else if(args.Length == 0 || args.Length > 2) {
+        if(args.Length == 0 || args.Length > 2) {
             Console.WriteLine(Constants.USAGE_HELP);
             Environment.Exit(0);
-        }
-        List<Token> tokens = scanner.initializeScanner(args[0]);
-        Console.WriteLine(Constants.TOKEN_TABLE_HEADER_FORMAT,
-            "TOKEN", "LINE", "COLUMN", "LEXEME"
-        );
-        foreach(Token token in tokens){
-            Console.WriteLine(
-                "{0,-12}{1,8}{2,8}\t{3}",
-                token.Type.ToString(),
-                token.Line,
-                token.Column,
-                token.Lexeme
+        } else if(args[0].Equals("-t")){
+            scanner.TestDriver();
+            Environment.Exit(0);
+        } else {
+            List<Token> tokens = scanner.initializeScanner(args[0]);
+            Console.WriteLine(Constants.TOKEN_TABLE_HEADER_FORMAT,
+                "TOKEN", "LINE", "COLUMN", "LEXEME"
             );
+            foreach(Token token in tokens){
+                Console.WriteLine(
+                    "{0,-12}{1,8}{2,8}\t{3}",
+                    token.Type.ToString(),
+                    token.Line,
+                    token.Column,
+                    token.Lexeme
+                );
+            }
         }
     }
 }
