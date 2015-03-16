6ad8cb9eb5b635693ab3231e2e75eaba658581cc
JesseStephenBrown <jessestephenbrown@gmail.com>
2015-03-16 15:55:33 -0600

    MMM, smells like a Vagrant

---
+++ bootstrapVagrant.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+echo "deb http://download.mono-project.com/repo/debian wheezy-apache24-compat main" | sudo tee -a /etc/apt/sources.list.d/mono-xamarin.list
+apt-get update
+apt-get install -y mono-complete
